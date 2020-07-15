0538bbef14836719a137c57981f1e192fc1f48ec
VANDAN <vandanmehta1231@gmail.com>
2020-07-15 14:15:58 +0530

    second

---
+++ email responsive html page/index.php
@@ -0,0 +1,309 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php $assetsPath = 'http://baymediasoft.be/email-templates/diwali/'; ?>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    <style type="text/css">
+        * {
+            -webkit-box-sizing: border-box;
+            -moz-box-sizing: border-box;
+            box-sizing: border-box;
+        }
+
+        body {
+            margin: 0;
+            padding: 0;
+            line-height: 1.2;
+            background-color: #ffffff;
+            font-size: 24px;
+            color: #000000;
+            font-family: "Arial", sans-serif;
+            font-weight: 400;
+        }
+
+        img {
+            max-width: 100%;
+            display: inline-block;
+            vertical-align: top;
+        }
+
+        p {
+            margin-top: 0;
+            font-size: 12pt;
+        }
+
+        h2,
+        h4 {
+            margin-bottom: 15px;
+            margin-top: 0;
+        }
+
+        .container {
+            width: 650px;
+            margin: 0 auto;
+        }
+
+        table {
+            margin: 0;
+            padding: 0;
+            width: 100% !important;
+
+        }
+
+        table td {
+            vertical-align: top;
+        }
+
+        .v-center {
+            vertical-align: middle;
+        }
+
+        .one_of_two {
+            width: 50%;
+        }
+
+        .one_of_three {
+            width: 33.333%;
+        }
+
+        .text-center {
+            text-align: center;
+        }
+
+        .text-right {
+            text-align: right;
+        }
+
+        .padding-15 {
+            padding: 15px;
+        }
+
+        .padding-50 {
+            padding: 0 80px;
+        }
+
+        .color-white {
+            color: #fff;
+        }
+
+        .text-pink {
+            font-size: 24px;
+            line-height: 30px;
+            color: #cc1a55;
+            text-transform: capitalize;
+        }
+
+        h1 {
+            text-transform: capitalize;
+            font-size: 24px;
+            font-weight: 400;
+        }
+
+        /*.tech-img {
+            width: 80%;
+            padding-top: 50px;
+            height:550px;
+        }
+*/
+        .contact h4 {
+            font-size: 14px;
+            color: #ebe6fb;
+            font-weight: bold;
+            text-transform: uppercase;
+        }
+
+        .bms-logo img {
+            width: 100%;
+        }
+
+        .contact p {
+            font-size: 14px;
+            color: #ebe6fb;
+        }
+
+        .contact a {
+            text-decoration: none;
+            color: #ebe6fb;
+        }
+
+        .diwali-msg {
+            color: #9f9bd8;
+            font-size: 18px;
+            letter-spacing: 1px;
+            line-height: 28px;
+        }
+
+        .diwali-msg a {
+            background-color: #d31b5b;
+            padding: 10px 20px;
+            text-decoration: none;
+            font-size: 14px;
+            color: #f0effc;
+            font-family: "Arial";
+            font-weight: bold;
+            text-transform: uppercase;
+            border-radius: 5px;
+        }
+
+        .social-links {
+            display: table;
+            margin: 0 auto;
+        }
+
+        .social-links ul {
+            /*text-align: center;
+            margin:0 auto;
+            padding:0;*/
+            width: 100%;
+            list-style: none;
+        }
+
+        .social-links ul li {
+            float: left;
+            list-style-type: none;
+            margin-right: 5px;
+        }
+
+        .footer-section .bg_blue {
+            color: #fff;
+            padding: 30px 0;
+        }
+
+        .footer-section .bg_blue h4 {
+            margin-bottom: 0 !important;
+        }
+
+
+        .copyright td {
+            padding: 20px;
+        }
+
+        .copyright p {
+            font-size: 10pt;
+            color: #fff;
+            margin-bottom: 0;
+        }
+
+        .copyright a {
+            text-decoration: none;
+            color: #fff;
+        }
+
+        .footer-section p {
+            font-size: 10pt;
+            color: #fff;
+        }
+
+        .one_column {
+            vertical-align: top;
+        }
+
+        .content-bottom {
+            background: url('http://baymediasoft.be/email-templates/diwali/assets/bg2.jpg');
+            background-size: 100% 100%;
+            background-repeat: no-repeat;
+        }
+
+        @media only screen and (max-width: 768px) {
+
+            .container {
+                width: 100% !important;
+            }
+
+            .res-t-p {
+                padding-top: 15px !important;
+            }
+
+            .res-b-p {
+                padding-bottom: 10px !important;
+            }
+
+        }
+    </style>
+</head>
+
+<body>
+<div class="container">
+
+    <table role="presentation" width="650" border="0" cellpadding="0" cellspacing="0" align="center" valign="top"
+           class="content-top">
+        <tr>
+            <td colspan="3" class="one_column text-center bms-logo">
+                <img width="100%" src="<?php echo $assetsPath ?>assets/top-2.gif"/>
+
+            </td>
+        </tr>
+
+    </table>
+
+    <table role="presentation" width="650" border="0" cellpadding="0" cellspacing="0" align="center" valign="top"
+           class="content-bottom">
+
+        <td background="http://baymediasoft.be/email-templates/diwali/assets/bg2.jpg" bgcolor="#211e53" width="600"
+            valign="top">
+           
+            <div>
+
+                <table border="0" cellpadding="0" cellspacing="0">
+                    <tr>
+                        <td colspan="3" class="padding-50 text-center one_of_three diwali-msg">
+                            <p>May the divine light of Diwali
+                                Spread into your Life Peace,
+                                Prosperity, Happiness and
+                                Good Health</p>
+                            <a href="https://www.baymediasoft.com/#hire-developer" target="_blank">Know Us</a>
+                        </td>
+                    </tr>
+
+                    <tr class="footer-section">
+                    <tr>
+                        <td colspan="3" class="padding-15 one_column text-center contact">
+                            <h4>Corporate Center</h4>
+                            <p>13907 Vanowen St, Unit 215 Van Nuys, CA 91405</p>
+                            <p>Phone : <a href="tel:+1-800-943-2204">+1-800-943-2204</a>, Email: <a
+                                    href="mailto:sales@baymediasoft.com">sales@baymediasoft.com</a></p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="3" align="center" class="text-center one_column social-links">
+                            <table width="100">
+                                <tr>
+                                    <td>&nbsp;</td>
+                                    <td><a href="https://www.facebook.com/Baymediasoft/" target="_blank"><img
+                                                src="http://baymediasoft.be/email-templates/diwali/assets/fb.png"/></a>
+                                    </td>
+                                    <td><a href="https://twitter.com/baymediasoft?lang=en" target="_blank"><img
+                                                src="http://baymediasoft.be/email-templates/diwali/assets/twitter.png"/></a>
+                                    </td>
+                                    <td><a href="https://in.linkedin.com/company/baymediasoft" target="_blank"><img
+                                                src="http://baymediasoft.be/email-templates/diwali/assets/linkedin.png"/>
+                                    </td>
+                                    <td><a href="https://www.behance.net/Baymediasoft" target="_blank"><img
+                                                src="http://baymediasoft.be/email-templates/diwali/assets/behance.png"/></a>
+                                    </td>
+                                    <td>&nbsp;</td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+
+                    <tr class="copyright">
+                        <td colspan="3" class="text-center one_column bg_grey">
+                            <p>© 2019 <a href="https://www.baymediasoft.com" target="_blank">Baymediasoft.com</a>. All
+                                rights reserved.</p>
+                        </td>
+                    </tr>
+                    </tr>
+                </table>
+            </div>
+        </td>
+
+</div>
+</body>
+
+</html>
